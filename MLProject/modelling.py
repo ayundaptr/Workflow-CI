3ed376c3a18c41a9491672daf1a63d7f18d90e8c
ayundaptr <ayunda6778@gmail.com>
2025-12-24 23:28:19 +0700

    Fix: Add nested=True to solve active run ID error

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,16 +4,20 @@ import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-# Load data hasil preprocessing
+# 1. Load data dengan path relatif agar aman di GitHub Actions
 df = pd.read_csv("../waterquality_preprocessing/waterquality_preprocessing.csv")
 X = df.drop(columns=["is_safe"]) 
 y = df["is_safe"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# 2. Setup MLflow
 mlflow.set_experiment("Water_Quality_Basic")
 mlflow.sklearn.autolog()
 
-with mlflow.start_run(run_name="Basic_Run"):
+# 3. Gunakan nested=True agar tidak bentrok dengan run dari 'mlflow run'
+with mlflow.start_run(run_name="Basic_Run", nested=True):
     model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)# Re-triggering Workflow for Skilled Level 
+    model.fit(X_train, y_train)
+
+# Re-triggering Workflow for Skilled Level
